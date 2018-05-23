f8fc5feba35f13c724d01106ffd9379f199ecfa7
Jason <dj@b612.email>
2018-05-23 12:55:42 +0800

    Update core.php
    should use a better 404 page later

--- files/core.php
+++ files/core.php
@@ -1,12 +1,12 @@
 <?php
-ini_set("display_errors", "On"); error_reporting(E_ALL);
-require_once 'tags.php';
-require_once 'conf.php';
-function mc_404()
-{
+@ini_set("display_errors", "On");   error_reporting(E_ALL);
+// why display errors in 404 pages... unless you run it in local?
+require_once('tags.php');
+require_once('conf.php');
+function mc_404(){
   header('HTTP/1.0 404 Not Found');
-  echo "<h1>404 Not Found</h1>";
-  echo "The page that you have requested could not be found.";
+  echo "<h2>404 Not Found</h2>";
+  echo "oops, the page you requested cannot be found.";
   exit();
 }
 ?>
